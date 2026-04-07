b34a39cdea6b4a47888b7b6939dd489ad35e7778
Subutai <subutai@sentient.io>
2026-04-06 21:02:47 -0700

    feat: install LetMeAllez script for auto-affiliate link conversion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(s,t,a,y22){s.Stay22=s.Stay22||{};s.Stay22.params={lmaID:'69d481c8e570ac3a76202d74'};var twenty=t.createElement(a);var two=t.getElementsByTagName(a)[0];twenty.async=1;twenty.src='https://scripts.stay22.com/letmeallez.js';two.parentNode.insertBefore(twenty,two)})(window,document,'script')`,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600&family=Source+Sans+3:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
